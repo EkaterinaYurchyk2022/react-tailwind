fb11d7b86e8fb523f356f7f2ce47eb417f33ddc6
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2023-06-15 20:29:59 +0300

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    screens: {
+      'xl': '1440px',
+      'md': '1024px'
+    },
+    extend: {
+      colors: {
+        'almost-white': 'hsl(0, 0%, 98%)',
+        'medium-gray': 'hsl(0, 0%, 41%)',
+        'almost-black': 'hsl(0, 0%, 8%)'
+      }
+    },
   },
   plugins: [],
 }
